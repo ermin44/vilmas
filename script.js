e4edcb26c1e544ac6718c1c4eae57227e19b1cfa
ermin44 <ermin49@live.dk>
2023-12-27 01:33:09 +0100

    gg

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    console.log("Anchor found: ", anchor.href); // This should log in the console
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
